853919de8dbaf653505b9771ec80449211a7346c
Michael Le <mnle529@gmail.com>
2019-12-05 01:23:50 -0800

    password generator

---
+++ script.js
@@ -0,0 +1,107 @@
+// Array of Special Characters
+var specialCharacters = [
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "-",
+    "=",
+    "+",
+    "'",
+    ",",
+    ".",
+    "?",
+    ";",
+    ":",
+    "<",
+    ">",
+    "[",
+    "]",
+    "{",
+    "}",
+    "_",
+    "|",
+    "`",
+    "~"
+]
+
+// Array of Numeric Characters
+var numericCharacters = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0"
+]
+
+// Array of Lowercase Characters
+var lowercaseCharacters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+]
+
+// Array of Uppercase Characters
+var uppercaseCharacters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+]
+
