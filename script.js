ce0f7dd843b8d82f2781454d4038acb7e6752bfc
Michael Le <mnle529@gmail.com>
2019-12-08 12:45:59 -0800

    week3-hw update

--- script.js
+++ script.js
@@ -105,3 +105,27 @@ var uppercaseCharacters = [
     "Z"
 ]
 
+// Asking users their password preference
+function getPasswordOptions() {
+    var length = parseInt(prompt(
+        "How many characters do you want your password to contain?"
+    ));
+
+    // This is to check if they entered a number to specify the length of the password
+    if (isNaN(length) === true) {
+        alert("Please specify the legnth of the password");
+        return;
+    }
+
+    // This is to check if the password is at least 8 characters long. If the statement is false then the prompt will end
+    if (length < 8) {
+        alert("Password needs to be atleast 8 characters long");
+        return;
+    }
+
+    // This is to check if the password is less tha 128 characters long. If the statement is false then the prompt will end
+    if (length > 128) {
+        alert("Password needs to be less than 129 characters long");
+        return; 
+    }
+}
