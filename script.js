362fab0f2eefbff97db0ba169ed3ecef67a94dfb
Michael Le <mnle529@gmail.com>
2019-12-08 20:28:25 -0800

    week3-hw update

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ var specialCharacters = [
     "|",
     "`",
     "~"
-]
+];
 
 // Array of Numeric Characters
 var numericCharacters = [
@@ -43,7 +43,7 @@ var numericCharacters = [
     "8",
     "9",
     "0"
-]
+];
 
 // Array of Lowercase Characters
 var lowercaseCharacters = [
@@ -73,7 +73,7 @@ var lowercaseCharacters = [
     "x",
     "y",
     "z"
-]
+];
 
 // Array of Uppercase Characters
 var uppercaseCharacters = [
@@ -103,7 +103,7 @@ var uppercaseCharacters = [
     "X",
     "Y",
     "Z"
-]
+];
 
 // Asking users their password preference
 function getPasswordOptions() {
@@ -130,36 +130,131 @@ function getPasswordOptions() {
     }
 
     // Add a Special Character to the password
-    var addSpecialCharacter = confirm(
+    var addSpecialCharacters = confirm(
         "Click OK to add a Special Character to the password"
     );
 
     // Add a Numeric Character to the password
-    var addNumericCharacter = confirm(
+    var addNumericCharacters = confirm(
         "Click OK to add a Numeric Number to the password"
     );
 
     // Add a Lowercase Character to the password
-    var addLowercaseCharacter = confirm(
+    var addLowercaseCharacters = confirm(
         "Click OK to add a Lowercase Character to the password"
     );
 
     // Add an Uppercase Character to the password
-    var addUppercaseCharacter = confirm(
+    var addUppercaseCharacters = confirm(
         "Click OK to add a Uppercase Character to the password"
     );
 
     // Making sure they have at least one of each character
     if (
-        addSpecialCharacter === false &&
-        addNumericCharacter === false &&
-        addLowercaseCharacter === false &&
-        addUppercaseCharacter === false 
+        addSpecialCharacters === false &&
+        addNumericCharacters === false &&
+        addLowercaseCharacters === false &&
+        addUppercaseCharacters === false 
     ) {
-        alert("Please add at least one of each character type")
+        alert("Please add at least one of each character type");
         return;
     }
 
+    // Creating a var to store the user character input
+    var passwordOptions = {
+        length: length,
+        addSpecialCharacters: addSpecialCharacters,
+        addNumericCharacters: addNumericCharacters,
+        addUppercaseCharacters: addUppercaseCharacters,
+        addLowercaseCharacters: addLowercaseCharacters
+    };
+
+    return passwordOptions;
+
+} // Closing {} of getPasswordOptions function
+
+  // Creating a var for the copy and generate element 
+  var generateBtn = document.querySelector("#generate");
+  var copyBtn = document.querySelector("#copy");
+  
+  // Click event for creating a password
+  generateBtn.addEventListener("click", writePassword);
+  // click event to save the password to a clipboard
+  copyBtn.addEventListener("click", copyToClipboard);
+
+
+
+// this function is to generate a random character from the arrays
+function getRandom(arr) {
+    var randIndex = Math.floor(Math.random() * arr.length);
+    var randElement = arr[randIndex];
+
+    return randElement;
+}
+
+function generatePassword() {
+    var options = getPasswordOptions();
     
+    var result = [];
+    var generatedChars = [];
+    var chosenChar = [];
+
+    // This is to add the array of special characters to the generatedChar array and push new random speical characters to the chosenChar.
+    if (options.addSpecialCharacters) {
+        generatedChars = generatedChars.concat(specialCharacters);
+        chosenChar.push(getRandom(specialCharacters));
+    }
+
+    // This is to add the array of numeric characters
+    if (options.addNumericCharacters) {
+        generatedChars = generatedChars.concat(numericCharacters);
+        chosenChar.push(getRandom(numericCharacters));
+    }
+
+    if (options.addLowercaseCharacters) {
+        generatedChars = generatedChars.concat(lowercaseCharacters);
+        chosenChar.push(getRandom(lowercaseCharacters));
+    }
+
+    if (options.addUppercaseCharacters) {
+        generatedChars = generatedChars.concat(uppercaseCharacters);
+        chosenChar.push(getRandom(uppercaseCharacters));
+    }
+
+    for (var i = 0; i < options.length; i++) {
+        var generatedPassword = getRandom(generatedChars);
+
+        result.push(generatedPassword);
+    }
+
+    for (var i = 0; i < chosenChar.length; i++) {
+        
+        result[i] = chosenChar[i];
+
+    return result.join("");
+    }
 
 }
+
+// Creating the password and copy to clipboard input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+    copyBtn.removeAttribute("disabled");
+    copyBtn.focus();
+  }
+  
+  function copyToClipboard() {
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.select();
+    document.execCommand("copy");
+  
+    alert(
+      "Your password " + passwordText.value + " was copied to your clipboard."
+    );
+  }
+
